22e87098cff9e7ef37a0cc592b6e39aeffd35f0b
FrankYin <djmax96945147@outlook.com>
2023-10-02 00:20:34 +0200

    Empty project created

---
+++ Gunner/Source/Gunner/Gunner.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Gunner.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Gunner, "Gunner" );
